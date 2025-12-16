30acae3da488bb0f9aef4ff38095ab1720a2d445
Zoe <183669693+Sikzyo@users.noreply.github.com>
2025-12-16 09:51:37 -0500

    feat: actualizar estructura de archivos y mejorar estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource-variable/manrope";
+import "@fontsource-variable/sora";
 import "./styles/main.css";
-import App from "./App.jsx";
+import App from "./pages/home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
