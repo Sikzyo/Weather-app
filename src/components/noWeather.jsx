02ea43d29e3efecb85f8fb06dbc715779440bc8b
Zoe <183669693+Sikzyo@users.noreply.github.com>
2025-12-23 14:32:08 -0500

    feat: arreglar estilos en componente noWeather

--- src/components/noWeather.jsx
+++ src/components/noWeather.jsx
@@ -4,7 +4,7 @@ export default function noWeather() {
   return (
     <section className="flex flex-col place-items-center gap-4">
       <CloudOff color="var(--color-gray-7)" size={100} />
-      <span className="text-2xl text-gray-7 text-center font-manrope font-semibold">
+      <span className="text-gray-7 font-manrope text-center text-2xl font-semibold">
         Agrega una ciudad para empezar.
       </span>
     </section>
